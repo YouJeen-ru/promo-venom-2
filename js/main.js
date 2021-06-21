533ec29e3a3d955996a2e5a24050d5149ae3c33d
YouJeen-ru <1.rouch@mail.ru>
2021-06-21 13:04:14 +0300

    Pop-up consult start coding

--- js/main.js
+++ js/main.js
@@ -10,4 +10,30 @@ menuButtonClose.addEventListener("click", () => {
   menuButtonClose.classList.remove("is-active");
 });
 
+//Pop-up consult
+
+const hideForm = document.querySelector('.hide-form')
+const orderTicket = document.querySelector('.order-ticket')
+const orderTrigger = document.querySelector('.order-trigger')
+const orderTicketForm = document.querySelector('.order-ticket__form')
+
+setTimeout(() => {
+  const heightForm = orderTicket.offsetHeight
+  hideForm.style.bottom = -heightForm + 'px'
+}, 1000)
+
+orderTrigger.addEventListener('click', () => {
+  hideForm.classList.toggle('hide-form-active')
+})
+
+orderTicketForm.addEventListener('change', (event) => {
+  const target = event.target
+  const label = target.labels[0]
+  if (label && target.value) {
+    label.classList.add('order-ticket__label-focus')
+  } else {
+    label.classList.remove('order-ticket__label-focus')
+  }
+})
+
 
